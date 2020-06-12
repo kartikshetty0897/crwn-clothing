54292f55035926e5aa82c65a954f99b1b77ee748
Kartik Shetty <kartik.shetty@t7e.in>
2020-06-12 22:43:57 +0530

    updated menu-items to redirect to the catalogue page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage = () => (
+const HatsPage = (props) => (
     <div>
         <h1>Hats Page</h1>
     </div>
