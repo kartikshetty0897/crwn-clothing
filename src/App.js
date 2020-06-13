0d506aec270a4edc52a8f879ebc29d02daec0d46
kartikshetty0897 <kartikshetty49@gmail.com>
2020-06-13 16:26:11 +0530

    added signup and singin pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SigninAndSignupPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
 
 const App = () => (
-    <div>
-        <Header />
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/shop" component={ShopPage} />
-        </Switch>
-    </div>
+  <div>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+      <Route path="/signin" component={SigninAndSignupPage} />
+    </Switch>
+  </div>
 );
 
 export default App;
